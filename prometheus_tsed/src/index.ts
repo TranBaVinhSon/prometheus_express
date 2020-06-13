625f4fbefe1f9f3c37cc2f2e2e21236da234a006
TranBaVinhSon <tranbavinhson@gmail.com>
2020-06-13 18:40:28 +0900

    fix stuff

--- prometheus_tsed/src/index.ts
+++ prometheus_tsed/src/index.ts
@@ -1,15 +1,15 @@
-import { $log, ServerLoader } from "@tsed/common";
+import { ServerLoader } from "@tsed/common";
 import { Server } from "./Server";
 
 async function bootstrap() {
   try {
-    $log.debug("Start server...");
+    console.log("Start server...");
     const server = await ServerLoader.bootstrap(Server);
 
     await server.listen();
-    $log.debug("Server initialized");
+    console.log("Server initialized");
   } catch (er) {
-    $log.error(er);
+    console.log(er);
   }
 }
 
